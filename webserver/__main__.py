ecfaec71b9f10b46dd8beaf4220742fc6698d915
lap1nou <lapinousexy@gmail.com>
2026-02-23 20:09:23 +0100

    v1.2

--- webserver/__main__.py
+++
@@ -1,9 +0,0 @@
-from webserver.config.config import AppConfig
-from webserver.tui.tui import TUI
-from webserver.cli.arguments import parse_arguments
-
-
-def main():
-    args = parse_arguments().parse_args()
-    config = AppConfig().config
-    TUI(args, config).run()
